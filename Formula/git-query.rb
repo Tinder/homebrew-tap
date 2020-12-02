def75853672e97302976b2de9b939cabc0fc8a23
Amin Ghadersohi <amin.ghadersohi@gotinder.com>
2020-12-01 23:39:50 -0800

    Update git-query.rb

--- Formula/git-query.rb
+++ Formula/git-query.rb
@@ -2,7 +2,7 @@ class GitQuery < Formula
   desc "Query, Copy and Sync files from a remote Git repo"
   homepage "https://github.com/Tinder/GitQuery"
   url "https://github.com/Tinder/GitQuery/archive/3.0.4.tar.gz"
-  sha256 "673e863c713c68137fb3d7efaf00601b1c1307732657e01c2fa6ec8093d240ce"
+  sha256 "c00b7c86dfffbdb0c9469787ff586f44d540c2a86b9f29cd00c4eecc25496072"
   
   depends_on "openjdk@8"
 
